ab04db2ed0e33a423d7e653cb95fe7450fd36abe
Cazka <can_oe@me.com>
2020-11-10 10:41:24 +0100

    should be working now

--- test/testa.js
+++ test/testa.js
@@ -1,25 +1,27 @@
 const DiepSocket = require('../');
 
 function join() {
-    const bot = new DiepSocket('diep.io/#2676073300954A40A226');
+    const bot = new DiepSocket('diep.io/#26A6375600E3F266F2');
     bot.on('open', () => console.log('open'));
     bot.on('accept', () => {
         console.log('accept');
-        bot.spawn('yes');
-
+        bot.spawn('akzaC');
         setInterval(() => {
+            bot.move();
+        }, 1000);
+        /*setInterval(() => {
             bot.moveTo({ x: 0, y: 0 });
         }, 20);
 
         bot.on('dead', () => {
             bot.spawn();
-        });
+        });*/
     });
     bot.on('close', (code, reason) => {
-        console.log(code, reason);
+        console.log('closed', code, reason);
     });
     bot.on('error', (err) => {
-        console.log(err);
+        console.log('err', err);
     });
 }
 join();
