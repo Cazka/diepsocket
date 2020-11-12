869c58705717b558b1001c71262909075bea47eb
Cazka <can_oe@me.com>
2020-11-12 15:55:01 +0100

    tests

--- test/testa.js
+++ test/testa.js
@@ -1,21 +1,20 @@
 const DiepSocket = require('../');
 
 function join() {
-    const bot = new DiepSocket('diep.io/#26A6375600E3F266F2');
+    const bot = new DiepSocket('diep.io/#26A657B60045E069FE');
     bot.on('open', () => console.log('open'));
     bot.on('accept', () => {
         console.log('accept');
-        bot.spawn('akzaC');
+        bot.spawn('DiepSocket');
         setInterval(() => {
-            bot.move();
-        }, 1000);
-        /*setInterval(() => {
-            bot.moveTo({ x: 0, y: 0 });
-        }, 20);
+            bot.moveTo({ x: 11150, y: 11150});
+            
+        }, 100);
 
         bot.on('dead', () => {
             bot.spawn();
-        });*/
+        });
+        bot.on('latency', (ping) => console.log(ping))
     });
     bot.on('close', (code, reason) => {
         console.log('closed', code, reason);
@@ -23,5 +22,7 @@ function join() {
     bot.on('error', (err) => {
         console.log('err', err);
     });
+    //bot.on('message', (data) => console.log(data));
 }
 join();
+//join();
