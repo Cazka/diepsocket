aeb384069c5c7a3e0c7527856144a020c33baeb8
Cazka <can_oe@me.com>
2020-11-12 23:04:39 +0100

    leaderboard example

---
+++ examples/leaderboard.js
@@ -0,0 +1,14 @@
+const DiepSocket = require('../');
+
+async function main() {
+    const link = await DiepSocket.findServerSync('4teams');
+
+    const bot = new DiepSocket(link);
+
+    bot.on('accept', () => {
+        console.log('link:', bot.link);
+        console.log(bot.leaderboard);
+        bot.close();
+    });
+}
+main();
