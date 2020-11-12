ec60bfc8803d6080884ae19a568d4f5741c853b0
Cazka <can_oe@me.com>
2020-11-12 23:47:06 +0100

    leaderboard parser added

--- src/protocol/lib/data/clientbound/update.js
+++ src/protocol/lib/data/clientbound/update.js
@@ -2,7 +2,7 @@ const FIELDS = {
     53: { name: 'y', datatype: 'vi' },
     48: { name: 'x', datatype: 'vi' },
     43: { name: 'angle', datatype: 'vi' },
-    39: { name: 'unknown', datatype: 'float'},
+    39: { name: 'unknown', datatype: 'float' },
 };
 const UPGRADE_ARRAY = [
     77,
@@ -180,7 +180,9 @@ function parseUpdate(parser) {
         parser.rv();
         id = Array.from(parser.buffer.slice(parser.at, creationIndex));
     }
-    return { id, parse };
+    //check if arena entity gets created
+    const leaderboard = parseLeaderboard(parser);
+    return { id, leaderboard, parse };
 }
 
 /**
@@ -215,14 +217,158 @@ function isDeleted(parser, id) {
     }
     return false;
 }
+/**
+ * Parses the Leaderboard. Credits to ABC
+ * @param {Parser} parser The parser
+ * @returns {Array} leaderboard The leaderboard
+ */
+function parseLeaderboard(parser) {
+    parser.at = 0;
+    //packet header
+    parser.vu();
+    //update id
+    parser.vu();
+    //deletions
+    for (let i = 0, n = parser.vu(); i < n; i++) {
+        //entity id
+        parser.vu();
+        parser.vu();
+    }
+    //updates-creations count
+    if (parser.vu() === 0) return;
+    //read first entity-id
+    parser.vu();
+    parser.vu();
+    //read update mode
+    if (parser.vu() !== 1 || parser.vu() !== 9 || parser.vu() !== 6 || parser.vu() !== 1) return;
+    //we have an arena entity creation
+    //------------------starting here------------------
+    const leaderboard = [...Array(10)].map(() => Object());
+    const COLORS = {
+        3: 'BLUE',
+        4: 'RED',
+        5: 'PURPLE',
+        6: 'GREEN',
+    };
+    const TANKS = [
+        'Tank', // 0
+        'Twin',
+        'Triplet',
+        'Triple Shot',
+        'Quad Tank',
+        'Octo Tank',
+        'Sniper',
+        'Machine Gun',
+        'Flank Guard',
+        'Tri-Angle',
+        'Destroyer', // 10
+        'Overseer',
+        'Overlord',
+        'Twin-Flank',
+        'Penta Shot',
+        'Assassin',
+        'Arena Closer',
+        'Necromancer',
+        'Triple Twin',
+        'Hunter',
+        'Gunner', // 20
+        'Stalker',
+        'Ranger',
+        'Booster', // 23
+        'Fighter',
+        'Hybrid',
+        'Manager',
+        'Mothership',
+        'Predator',
+        'Sprayer',
+        '', // deleted
+        'Trapper',
+        'Gunner Trapper',
+        'Overtrapper',
+        'Mega Trapper',
+        'Tri-Trapper',
+        'Smasher',
+        '', // deleted
+        'Landmine',
+        'Auto Gunner',
+        'Auto 5',
+        'Auto 3',
+        'Spread Shot',
+        'Streamliner',
+        'Auto Trapper',
+        'Dominator', // Destroyer
+        'Dominator', // Gunner
+        'Dominator', // Trapper
+        'Battleship',
+        'Annihilator',
+        'Auto Smasher',
+        'Spike',
+        'Factory',
+        'Ball',
+        'Skimmer',
+        'Rocketeer',
+    ];
+    //leaderX
+    parser.float();
+    //tankIds
+    for (let i = 0; i < 10; i++) {
+        const tank_id = parser.vi();
+        leaderboard[i].tank = TANKS[tank_id];
+    }
+    //unknown
+    parser.vu();
+    parser.float();
+    parser.float();
+    //labels
+    for (let i = 0; i < 10; i++) {
+        const label = parser.string();
+        if (label.length > 0) leaderboard[i].label = label;
+    }
+    //unknown
+    parser.vu();
+    parser.float();
+    //names
+    for (let i = 0; i < 10; i++) {
+        const name = parser.string();
+        leaderboard[i].name = name;
+    }
+    //leaderboard length
+    const leaderboard_length = parser.vu();
+    //color
+    for (let i = 0; i < 10; i++) {
+        const color = parser.vu();
+        leaderboard[i].color = COLORS[color];
+    }
+    //leaderY
+    parser.float();
+    //bottomY
+    parser.float();
+    //unknown
+    parser.vu();
+    //rightX
+    parser.float();
+    //topY
+    parser.float();
+    //leftX
+    parser.float();
+    //unknown
+    parser.vu();
+    //scores
+    for (let i = 0; i < 10; i++) {
+        const score = parser.float();
+        leaderboard[i].score = Math.floor(score);
+    }
+    return leaderboard.slice(0, leaderboard_length);
+}
 
 module.exports = {
     id: 0,
     type: 'update',
     parse(parser) {
-        const { id, parse } = parseUpdate(parser);
+        const { id, leaderboard, parse } = parseUpdate(parser);
         return {
             id,
+            leaderboard,
             parse,
         };
     },
