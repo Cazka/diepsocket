528c0b6483825f6dd18980d3bdf8de1f63b87260
Cazka <can_oe@me.com>
2020-11-13 13:27:22 +0100

    better parsing of region

--- src/protocol/lib/data/clientbound/server_info.js
+++ src/protocol/lib/data/clientbound/server_info.js
@@ -3,18 +3,21 @@ module.exports = {
     type: 'server_info',
     parse(parser) {
         const gamemode = parser.string();
-        const region = parser.string();
+        const host_region = parser.string();
+        const [host, region] = host_region.split['-'];
         return {
             gamemode,
+            host,
             region,
         };
     },
     build(builder) {
-        const { gamemode, region } = builder._packet.content;
+        const { gamemode, host, region } = builder._packet.content;
+        const host_region = host + '-' + region;
 
         builder.vu(this.id);
         builder.string(gamemode);
-        builder.string(region);
+        builder.string(host_region);
         return builder.out();
     },
 };
